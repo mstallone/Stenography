8864351b3b40acdec4ce42d61720492f9876c707
Matthew Stallone <mstallone@headroyce.org>
2017-02-28 23:27:55 -0800

    Finish bitwise encoding

---
+++ Stenography/Stenography.h
@@ -0,0 +1,25 @@
+//
+//  Stenography.h
+//  Stenography
+//
+//  Created by Matthew Stallone on 2/28/17.
+//  Copyright © 2017 Matthew Stallone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+@interface Stenography : NSObject {
+    NSInteger pixelsWide, pixelsHigh;
+    unsigned char *pixels;
+}
+
+@property (nonatomic) NSBitmapImageRep *sourceBitmapRep;
+@property (nonatomic) NSWindow *window;
+
+- (id)initWithWindow:(NSWindow *)window;
+- (void) setSource:(NSImage *)source;
+- (NSImage *)encodeWithMessage:(NSString *)message;
+- (NSString *)decode;
+
+@end
--- Stenography/ViewController.m
+++ Stenography/ViewController.m
@@ -12,16 +12,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
+    engine = [[Stenography alloc] initWithWindow:self.view.window];
 }
-
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
 }
 
+- (IBAction)chooseSource:(id)sender {
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    [openPanel setCanChooseDirectories:NO];
+    [openPanel setAllowsMultipleSelection:NO];
+    [openPanel setAllowedFileTypes:[NSImage imageTypes]];
+    [openPanel setTitle:@"Please choose your source image"];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [openPanel runModal];
+        
+        NSImage *source = [[NSImage alloc] initWithContentsOfURL:[openPanel URL]];
+        if (source != nil) {
+            [engine setSource:source];
+            [_sourceView setImage:source];
+        }
+    });
+}
+
+- (IBAction)sourceDragged:(id)sender {
+    [engine setSource:_sourceView.image];
+}
+
+- (IBAction)saveOutput:(id)sender {
+    if (_outputView.image == nil) {
+        NSAlert *noOutputAlert = [[NSAlert alloc] init];
+        [noOutputAlert setMessageText:@"There is no image to save!"];
+        [noOutputAlert setInformativeText:@"Please encode a message into the source image to save the resulting file."];
+        [noOutputAlert addButtonWithTitle:@"Close"];
+        [noOutputAlert beginSheetModalForWindow:self.view.window completionHandler:nil];
+    }
+    
+    //TODO
+}
+
+- (IBAction)resetData:(id)sender {
+    [engine setSource:nil];
+    
+    _outputView.image = nil;
+    _sourceView.image = nil;
+}
+
+- (IBAction)encode:(id)sender {
+    _outputView.image = [engine encodeWithMessage:[_messageField stringValue]];
+}
+
+- (IBAction)decode:(id)sender {
+    [_messageField setStringValue:[engine decode]];
+}
 
 @end
